dab69ec708d97c3ebddfdaa5fc2445009af6bb6b
Zoran Đukić <zorandjukic.os@gmail.com>
2022-02-26 23:20:24 +0100

    Crated footer, header and basic shape for list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ListEmployeeComponent from './components/ListEmployeeComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <HeaderComponent />
+        <div className= "container">
+          <Switch>
+              <Route exact path = "/" component = {ListEmployeeComponent}></Route>
+              <Route path = "/employees" component = {ListEmployeeComponent}></Route>
+            </Switch>
+        </div>
+        <FooterComponent />
+        </Router>
     </div>
   );
 }
