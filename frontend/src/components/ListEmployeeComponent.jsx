aced90a36fd1096f5327e5fe76c626cf857d4f77
Zoran Đukić <zorandjukic.os@gmail.com>
2022-02-26 23:20:51 +0100

    Created list and axios for REST

---
+++ frontend/src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+import EmployeeService from '../services/EmployeeService'
+
+const ListEmployeeComponent = () => {
+
+    //What you change, how = curret value
+    const [employees, setEmployees] = useState([])
+
+
+
+    return (
+        <div className="container">
+            <h2 className="text-center"> List Employees </h2>
+            <Link to="/add-employee" className="btn btn-primary mb-2" > Add Employee </Link>
+            <table className="table table-bordered table-striped">
+                <thead>
+                    <th> Employee Id </th>
+                    <th> Employee First Name </th>
+                    <th> Employee Last Name </th>
+                    <th> Employee Email Id </th>
+                    <th> Actions </th>
+                </thead>
+                <tbody>
+                    {
+                        employees.map(
+                            employee =>
+                                <tr key={employee.id}>
+                                    <td> {employee.id} </td>
+                                    <td> {employee.firstName} </td>
+                                    <td>{employee.lastName}</td>
+                                    <td>{employee.emailId}</td>
+                                    <td>
+                                        <Link className="btn btn-info" to={`/edit-employee/${employee.id}`} >Update</Link>
+                                    </td>
+                                </tr>
+                        )
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default ListEmployeeComponent
