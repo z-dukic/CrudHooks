de45f3b7d906abf7d3d11b3dbcf2f8d3d262ae20
Zoran Đukić <zorandjukic.os@gmail.com>
2022-02-27 22:12:47 +0100

    End of hooks employee app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
@@ -13,15 +13,16 @@ function App() {
     <div>
       <Router>
         <HeaderComponent />
-        <div className= "container">
+        <div className="container">
           <Switch>
-              <Route exact path = "/" component = {ListEmployeeComponent}></Route>
-              <Route path = "/employees" component = {ListEmployeeComponent}></Route>
-              <Route path = "/add-employee" component = {AddEmployeeComponent} ></Route>
-            </Switch>
+            <Route exact path="/" component={ListEmployeeComponent}></Route>
+            <Route path="/employees" component={ListEmployeeComponent}></Route>
+            <Route path="/add-employee" component={AddEmployeeComponent} ></Route>
+            <Route path="/edit-employee/:id" component={AddEmployeeComponent}></Route>
+          </Switch>
         </div>
         <FooterComponent />
-        </Router>
+      </Router>
     </div>
   );
 }
--- frontend/src/components/ListEmployeeComponent.jsx
+++ frontend/src/components/ListEmployeeComponent.jsx
@@ -5,27 +5,36 @@ import EmployeeService from '../services/EmployeeService'
 
 const ListEmployeeComponent = () => {
 
-    useEffect(() => {
-
-        EmployeeService.getAllEmployees().then((response) =>{
-            setEmployees(response.data)
-            console.log(response.data)
-        }).catch(error => {
-            console.log(error);
-        })
-
-    }, [])
-
-
-    //What you change, how = curret value
     const [employees, setEmployees] = useState([])
 
+    useEffect(() => {
 
+        getAllEmployees();
+    }, [])
+
+    const getAllEmployees = () => {
+        EmployeeService.getAllEmployees().then((response) => {
+            setEmployees(response.data)
+            console.log(response.data);
+        }).catch(error =>{
+            console.log(error);
+        })
+    }
+
+    const deleteEmployee = (employeeId) => {
+       EmployeeService.deleteEmployee(employeeId).then((response) =>{
+        getAllEmployees();
+
+       }).catch(error =>{
+           console.log(error);
+       })
+        
+    }
 
     return (
-        <div className="container">
-            <h2 className="text-center"> List Employees </h2>
-            <Link to="/add-employee" className="btn btn-primary mb-2" > Add Employee </Link>
+        <div className = "container">
+            <h2 className = "text-center"> List Employees </h2>
+            <Link to = "/add-employee" className = "btn btn-primary mb-2" > Add Employee </Link>
             <table className="table table-bordered table-striped">
                 <thead>
                     <th> Employee Id </th>
@@ -38,17 +47,18 @@ const ListEmployeeComponent = () => {
                     {
                         employees.map(
                             employee =>
-                                <tr key={employee.id}>
-                                    <td> {employee.id} </td>
-                                    <td> {employee.firstName} </td>
-                                    <td>{employee.lastName}</td>
-                                    <td>{employee.emailId}</td>
-                                    <td>
-                                        <Link className="btn btn-info" to={`/edit-employee/${employee.id}`} >Update</Link>
-                                    </td>
-                                </tr>
+                            <tr key = {employee.id}> 
+                                <td> {employee.id} </td>
+                                <td> {employee.firstName} </td>
+                                <td>{employee.lastName}</td>
+                                <td>{employee.emailId}</td>
+                                <td>
+                                    <Link className="btn btn-info" to={`/edit-employee/${employee.id}`} >Update</Link>
+                                    <button className = "btn btn-danger" onClick = {() => deleteEmployee(employee.id)}
+                                    style = {{marginLeft:"10px"}}> Delete</button>
+                                </td>
+                            </tr>
                         )
-                        
                     }
                 </tbody>
             </table>
